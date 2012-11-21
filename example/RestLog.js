146835d3cb5dd9756e09ea2727608398ee0f947e
xiaoz <24409917@qq.com>
2012-11-21 14:42:13 +0800

    a

--- example/RestLog.js
+++
@@ -1,7 +0,0 @@
-module.exports._config = require('./config/log.conf.js');
-var rrest = require('../');
-restlog.debug('debug');
-restlog.info('info')
-restlog.warn('warn');
-restlog.error('error');
-//查看 ../mylogs/console.log文件即可
